705c8630a5c4a2d54a844aaf170e9cc2daf7038b
gabrielknot <gcftutos@gmail.com>
2021-03-18 00:48:48 -0300

    Adding form control on username

--- Angular-Frontend/src/app/login/login.component.ts
+++ Angular-Frontend/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { FormGroup, FormBuilder, Validators }from '@angular/forms';
 
+import { User } from '../models/user.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,11 +10,16 @@ import { FormGroup, FormBuilder, Validators }from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 	public form: FormGroup;
+	public users: User[] = [];
   constructor(private fb:FormBuilder) {
 	this.form = this.fb.group({userName:['',Validators.required]
 	})
   }
-
+	addUser(){
+		const userName = this.form.controls['userName'].value;
+		this.users.push( new User(userName,false));
+		this.form.reset();
+	}
   ngOnInit(): void {
   }
 
